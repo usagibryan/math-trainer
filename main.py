71fc420bc9c4bd4aab6b6667ef056aa776183ab0
usagibryan <3444929+usagibryan@users.noreply.github.com>
2023-04-02 20:18:05 -0400

    Add comments

--- main.py
+++ main.py
@@ -10,24 +10,34 @@ def play_game():
     correct_guesses = 0
     rounds = 0
     print("Welcome to Math Trainer! Press Q to quit!")
+    
     while True:
         num1 = get_random_number()
         num2 = get_random_number()
+
+        # Prevent subtracting a larger number from a smaller one.
         if num1 < num2:
             num1, num2 = num2, num1
         answer = subtract_numbers(num1, num2)
         guess = input(f"What is {num1} - {num2}? ")
+
+        # Allow user to quit the program.
         if guess.lower() == "q":
             print()
             print("Thank you for playing.")
             sys.exit()
+
+        # Input validation, user should only enter numbers.
         while True:
             try:
                 guess = int(guess)
             except ValueError:
                 print("Invalid input. Please enter a number.")
                 print()
+                # Ask for the answer to the same question agian.
                 guess = input(f"What is {num1} - {num2}? ")
+
+                # Allow user to quit even during input validation.
                 if guess.lower() == "q":
                     print()
                     print("Thank you for playing.")
@@ -35,6 +45,7 @@ def play_game():
                 continue
             else:
                 break
+        
         if int(guess) == answer:
             correct_guesses += 1
             rounds += 1
